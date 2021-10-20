b38b572e9a485b47e99a7b625f9927f13ffcb413
Yago Pajariño <yago977@hotmail.com>
2021-10-20 00:06:14 -0300

    add ui

--- scripts.js
+++ scripts.js
@@ -9,16 +9,75 @@ function playRound(playerSelection, computerSelection) {
     let c = computerSelection.toLowerCase()
     wins = {"rock" : "scissors", "paper" : "rock", "scissors" : "paper"}
     if (p == c) {
-        return [0,"Round tied"]
+        return 0
     }
     else if (wins[p] == c) {
-        return [1,`You Won! ${playerSelection} beats ${computerSelection}`]
+        return 1
     }
     else {
-        return [-1,`You Lose! ${computerSelection} beats ${playerSelection}`]
+        return 2
     }
 }
 
+let buttons = document.querySelectorAll(".option_btn")
+buttons.forEach(function(btn) {
+    btn.addEventListener("click", buttonPlay.bind(null,btn))
+})
+
+function buttonPlay(btn) {
+    let userSel = btn.id
+    let cPlay = computerPlay()
+    let round_winner = playRound(userSel,cPlay)
+    change_results(round_winner, userSel, cPlay)
+    check_winner()
+}
+
+function change_results(r, p, c) {
+    let message = document.createElement("li")
+    let res_div = document.querySelector(".resBullets")
+    let userPoints = document.querySelector("#user_points")
+    let compPoints = document.querySelector("#computer_points")
+    if(r === 0) {
+        message.textContent=`Draw! You chose ${p} and computer ${c}`
+    }
+    else if (r === 1){
+        message.textContent=`You win! You chose ${p} and computer ${c}`
+        n = parseInt(userPoints.textContent)
+        n += 1
+        userPoints.textContent = n
+        
+    }
+    else {
+        message.textContent=`You lose! You chose ${p} and computer ${c}`
+        n = parseInt(compPoints.textContent)
+        n += 1
+        compPoints.textContent = n
+    }
+    res_div.appendChild(message)
+}
+
+function check_winner() {
+    let userPoints = document.querySelector("#user_points")
+    let compPoints = document.querySelector("#computer_points")
+    let winDiv = document.querySelector(".winnerMessage")
+    if (userPoints.textContent === "5") {
+        winDiv.textContent = "You Won!!"
+        removeListeners()
+    }
+    else if (compPoints.textContent === "5") {
+        winDiv.textContent = "You Lost!!"
+        removeListeners()
+    }
+    return 0
+}
+
+function removeListeners() {
+    buttons.forEach(function(btn) {
+        btn.replaceWith(btn.cloneNode(true))
+    })
+}
+
+/* Deprecated
 function game() {
     let pScore = 0
     for (let x = 0; x < 5; x++) {
@@ -41,4 +100,6 @@ function game() {
     else {
         console.log(`You lost the game!`)
     }
-}
+} */
+
+
